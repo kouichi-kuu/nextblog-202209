956ca31adb640c027b9f19fec015205a78dcbf02
kouichi-kuu <60015181+kouichi-kuu@users.noreply.github.com>
2022-10-09 12:36:19 +0900

    20221009変更3

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
+import PostBody from 'components/post-body'
 
 export default function About(){
     return (
         <Container>
         <Hero title="About" subtitle="About development activities" />
+        <PostBody>
         <p>
               Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
             </p>
@@ -19,6 +21,7 @@ export default function About(){
             <p>
               今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
             </p>
+            </PostBody>
         </Container>
     )
 }
