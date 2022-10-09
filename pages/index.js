d644f572317d85e4884185e174b5abf5a54e59c7
kouichi-kuu <60015181+kouichi-kuu@users.noreply.github.com>
2022-10-09 11:41:24 +0900

    20221009変更2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Hero from 'components/hero'
+import Container from 'components/container'
 
 export default function Home(){
   return (
+    <Container>
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
+    </Container>
   )
 }
